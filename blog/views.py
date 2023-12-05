6ac52e0fc508687de9a57329e85e5be5e853975b
IamGOOD <e.rukshenas@gmail.com>
2023-12-04 21:51:08 -0500

    Added static files

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.views.generic import ListView
 from .models import Post
 class BlogListView(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = 'blog/home.html'
 
