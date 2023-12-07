a69aae0c0a505bda5b0024dc776584429b8a0cab
IamGOOD <e.rukshenas@gmail.com>
2023-12-06 22:06:41 -0500

    Added simple form for user add post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 class BlogListView(ListView):
     model = Post
@@ -9,3 +10,7 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = ['author', 'title', 'content']
