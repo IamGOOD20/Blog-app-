399ce56189d167fc9c37b26d5f27c34346727b95
IamGOOD <e.rukshenas@gmail.com>
2023-12-17 10:12:33 -0500

    Implemented Sign Up

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,7 @@ from .models import Post
 # 1234512345
 # blogapp
 
+# djack1234512345
+# 1234512345
+
 admin.site.register(Post)
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # local
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
